5052b1a9fb03bb3f1147891b4ba6f27e49c0a95d
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-02-10 04:08:49 -0600

    assignment part 2

--- ITMD-521/assignment-01/assignment-01.scala
+++ ITMD-521/assignment-01/assignment-01.scala
@@ -1,9 +1,9 @@
 package main.scala.assignment
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-/**
- * Usage: MnMcount <mnm_file_dataset>
- */
+import org.apache.spark.sql.types._
+
+
 object assignment{
 def main(args: Array[String]) {
      val spark = SparkSession.builder.appName("Assignment1").getOrCreate()
@@ -12,10 +12,37 @@ def main(args: Array[String]) {
      sys.exit(1)
      }
 
-     val DivvyFile = args(0)
-     val Infer_DF = spark.read.format("csv") .option("header", "true") .option("inferSchema", "true") .load(DivvyFile)
-     Infer_DF.show(20)
+     val data_source = args(0)
+     val infer_divvy_df = spark.read.format("csv") .option("header", "true") .option("inferSchema", "true") .load(data_source)
+     infer_divvy_df.show(20)
+     infer_divvy_df.printSchema()
 
 
-}
+     val struct_schema = StructType(Array(StructField("trip_id", IntegerType),
+     StructField("starttime", StringType),
+     StructField("stoptime", StringType),
+     StructField("bikeid", IntegerType),
+     StructField("tripduration", IntegerType),
+     StructField("from_station_id", IntegerType),
+     StructField("from_station_name", StringType),
+     StructField("to_station_id", IntegerType),
+     StructField("to_station_name", StringType),
+     StructField("usertype", StringType),
+     StructField("gender", StringType),
+     StructField("birthyear", IntegerType)))
+
+     val struct_divvy_df = spark.read.schema(struct_schema).format("csv").option("header", "true").option("structureSchema", "true").load(data_source)
+     struct_divvy_df.show(10)
+     struct_divvy_df.printSchema()
+
+
+     val ddl_schema = "trip_id INT,starttime STRING,stoptime STRING,bikeid INT,tripduration INT,from_station_id INT,from_station_name STRING,to_station_id INT,to_station_name STRING,usertype STRING,gender STRING,birthyear INT"
+     val ddl_df = (spark.read.schema(ddl_schema).format("csv")).option("header", "true").load(data_source)
+     ddl_df.show()
+     ddl_df.printSchema()
+
+
+    val select_gender_df = (infer_divvy_df.select("gender", "to_station_id", "to_station_name").where(infer_divvy_df.gender == "Male").groupBy("to_station_id").count())
+     select_gender_df.show(10)
+     }
 }
