9ab29f0237ab7ce9d1c41c3b12e021a1e16143eb
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-02-09 21:42:44 -0600

    Assignment 1

--- ITMD-521/assignment-01/assignment-01.py
+++ ITMD-521/assignment-01/assignment-01.py
@@ -13,8 +13,9 @@ if __name__ == "__main__":
         sys.exit(-1)
 
     spark = (SparkSession.builder.appName("Assignment 1").getOrCreate())
-    Divvy_File = sys.argv[1]
+    data_source = sys.argv[1]
 
-    infer_divvy_df = (spark.read.format("csv").option("header","true").option("inferSchema","true").load(Divvy_File))
+    infer_divvy_df = (spark.read.format("csv").option("header","true").option("inferSchema","true").load(data_source))
     infer_divvy_df.show(n=10, truncate=False)
     infer_divvy_df.printSchema()
+    parquet_path = infer_divvy_df.write.format("parquet").save("/home/vagrant/srajulu/ITMD-521/assignment-01/example-data")
