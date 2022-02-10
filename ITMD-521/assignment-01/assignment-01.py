c54fec428784c400a6c8484c962152c64f0c29d5
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-02-09 21:55:37 -0600

    Assignment 1

--- ITMD-521/assignment-01/assignment-01.py
+++ ITMD-521/assignment-01/assignment-01.py
@@ -16,9 +16,13 @@ if __name__ == "__main__":
     spark = (SparkSession.builder.appName("Assignment 1").getOrCreate())
     data_source = sys.argv[1]
 
+## Inferring Schema
+
     infer_divvy_df = (spark.read.format("csv").option("header","true").option("inferSchema","true").load(data_source))
     infer_divvy_df.show(n=10, truncate=False)
     infer_divvy_df.printSchema()
     parquet_path = infer_divvy_df.write.format("parquet").save("/home/vagrant/srajulu/ITMD-521/assignment-01/example-data/divvy-2015-q1")
     parquet_file = spark.read.format("parquet").load("/home/vagrant/srajulu/ITMD-521/assignment-01/example-data/divvy-2015-q1")
     parquet_file.show(n=10, truncate=False)
+
+## Programmatically creating and attaching a schema using StructFields
