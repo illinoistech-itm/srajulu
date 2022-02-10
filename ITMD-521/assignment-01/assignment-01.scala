01ebb1410495954871fc9f5601d9e94d7aa5781d
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-02-10 04:28:19 -0600

    assignment part 2

--- ITMD-521/assignment-01/assignment-01.scala
+++ ITMD-521/assignment-01/assignment-01.scala
@@ -42,7 +42,7 @@ def main(args: Array[String]) {
      ddl_df.printSchema()
 
 
-    val select_gender_df = infer_divvy_df.select("gender","to_station_id","to_station_name","Count").where(infer_divvy_df.gender == "Male").groupBy("to_station_id").sum("Count").orderBy(desc("sum(Count)"))
+    val select_gender_df = struct_divvy_df.select("gender","to_station_id","to_station_name","Count").where(struct_divvy_df.gender == "Male").groupBy("to_station_id").sum("Count").orderBy(desc("sum(Count)"))
     select_gender_df.show(10, true)
 
      }
