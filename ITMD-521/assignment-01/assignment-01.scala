098aa93b205205cac98d54e30721052e8082f5f6
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-02-10 01:21:10 -0600

    Assignment 1

--- ITMD-521/assignment-01/assignment-01.scala
+++ ITMD-521/assignment-01/assignment-01.scala
@@ -1,22 +1,19 @@
+package main.scala.assignment1 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
 object assignment {
-def main(args: Array[String]) {
-val spark = SparkSession .builder
-     .appName("assignment1")
-     .getOrCreate()
+    def main(args: Array[String]) {
+    val spark = SparkSession .builder.appName("assignment1").getOrCreate()
+    if (args.length < 1) {
+        print("Usage: assignment1 <mnm_file_dataset>")
+        sys.exit(1)
+        }
 
-if (args.length < 1) {
-print("Usage: assignment1 <mnm_file_dataset>")
- sys.exit(1)
+    }
+    val DivvyFile = args(0)
+
+    val Infer_DF = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load(DivvyFile)
+    Infer_DF.show()
+    Infer_DF.printSchema()
 }
-
-val DivvyFile = args(0)
-
-val Infer_DF = spark.read.format("csv") 
-.option("header", "true") 
-.option("inferSchema", "true") 
-.load(DivvyFile)
-Infer_DF.show()
-Infer_DF.printSchema()
