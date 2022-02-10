6e6c54e54c9d9280960bacd9bef6495bb4434b9e
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-02-09 23:08:54 -0600

    Assignment 1

--- ITMD-521/assignment-01/assignment-01.py
+++ ITMD-521/assignment-01/assignment-01.py
@@ -2,6 +2,7 @@
 #from optparse import Option
 #from os import truncate
 from os import truncate
+from statistics import mode
 import sys
 
 from pyspark.sql import SparkSession
@@ -21,9 +22,9 @@ if __name__ == "__main__":
     infer_divvy_df = (spark.read.format("csv").option("header","true").option("inferSchema","true").load(data_source))
     infer_divvy_df.show(n=10, truncate=False)
     infer_divvy_df.printSchema()
-    parquet_path = infer_divvy_df.write.format("parquet").save("/home/vagrant/srajulu/ITMD-521/assignment-01/example-data/divy-2015")
-    #parquet_file = spark.read.format("parquet").load("/home/vagrant/srajulu/ITMD-521/assignment-01/example-data/divy-2015")
-    #parquet_file.show(n=10, truncate=False)
+    parquet_path = infer_divvy_df.write.format("parquet").save("/home/vagrant/srajulu/ITMD-521/assignment-01/example-data/divy-2015.parquet")
+    parquet_file = spark.read.format("parquet").load("/home/vagrant/srajulu/ITMD-521/assignment-01/example-data/divy-2015")
+    parquet_file.show(n=10, truncate=False)
 
 ## Programmatically creating and attaching a schema using StructFields
 
@@ -43,7 +44,16 @@ if __name__ == "__main__":
     struct_divvy_df = (spark.read.schema(struct_schema).format("csv")).option("header","true").option("structureSchema","true").load(data_source)
     struct_divvy_df.show()
     struct_divvy_df.printSchema()
+    
 
 ## Attaching a schema via a DDL
 
-  #  ddl_divvy_df = 
+    ddl_schema =  "trip_id INT,starttime STRING,stoptime STRING,bikeid INT,tripduration INT,from_station_id INT,from_station_name STRING,to_station_id INT,to_station_name STRING,usertype STRING,gender STRING,birthyear INT"
+    ddl_df = (spark.read.schema(ddl_schema).format("csv")).option("header", "true").load(data_source)
+    ddl_df.show()
+    ddl_df.printSchema()
+
+## Select
+
+    select_gender_df = (infer_divvy_df.select("gender", "to_station_id", "to_station_name").where(infer_divvy_df.gender == 'Male').groupBy("to_station_id").count())
+    select_gender_df.show(n=10, truncate=False)
