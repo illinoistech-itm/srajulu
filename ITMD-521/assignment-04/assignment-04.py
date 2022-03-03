ee25684af49293d06dfbbcc05aae5437592f573f
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-03-02 19:48:27 -0600

    assignmen t4

--- ITMD-521/assignment-04/assignment-04.py
+++ ITMD-521/assignment-04/assignment-04.py
@@ -1,7 +1,9 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
+import findspark
 
 spark = SparkSession.builder.appName("Assignment04").getOrCreate()
+findspark.add_packages('mysql:mysql-connector-java:8.0.11')
 
 # Read Option 1: Loading data from a JDBC source using load method
 jdbcDF1 = (spark.read
