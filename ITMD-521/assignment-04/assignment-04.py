7e04182bab9a90cc911bfb68f2d3a16187ae1061
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-03-02 20:13:02 -0600

    assignment 4 fix

--- ITMD-521/assignment-04/assignment-04.py
+++ ITMD-521/assignment-04/assignment-04.py
@@ -6,4 +6,4 @@ spark = SparkSession.builder.appName("Assignment04").getOrCreate()
 
 
 # Read Option 1: Loading data from a JDBC source using load method
-jdbcDF1 = (spark.read.format("jdbc").option("url", "jdbs:mysql://localhost:3306;databaseName:employees").option("dbtable", "employees").option("user", "worker").option("password", "cluster").load())
+jdbcDF1 = (spark.read.format("jdbc").option("url", "jdbc:mysql://localhost:3306;databaseName:employees").option("dbtable", "employees").option("user", "worker").option("password", "cluster").load())
