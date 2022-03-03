33cb5fd1eee1d67934c82545de459639c09d1503
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-03-03 03:26:52 -0600

    Assignment q4 fix

--- ITMD-521/assignment-04/assignment-04.py
+++ ITMD-521/assignment-04/assignment-04.py
@@ -11,14 +11,14 @@ spark = SparkSession\
 
 JDBC_DF_TEMP = spark.read.format("jdbc").option("url","jdbc:mysql://localhost/employees").option("driver","com.mysql.jdbc.Driver").option("dbtable","employees").option("user","worker").option("password","cluster").load().createOrReplaceTempView("Employees")
 JDBC_DF = spark.read.format("jdbc").option("url","jdbc:mysql://localhost/employees").option("driver","com.mysql.jdbc.Driver").option("dbtable","employees").option("user","worker").option("password","cluster").load()
-
+JDBC_DF.show()
 #Assignment Part 1
 # Q1 - Display the count of the number of records in the DF
 Q1_DF=spark.sql("Select count(*) from Employees")
 Q1_DF.show()
 
 # Q2 - Display the schema of the Employees Table from the DF
-JDBC_DF_TEMP.printSchema()
+JDBC_DF.printSchema()
 
 # Q3 - Create a DataFrame of the top 10,000 employee salaries (sort DESC) from the salaries table
 Q3 = spark.read.format("jdbc")\
