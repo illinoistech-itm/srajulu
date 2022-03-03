7ed3fced45cf51903c3a4442cde83cfca1c92d57
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-03-02 21:58:28 -0600

    assignment 4 fix

--- ITMD-521/assignment-04/assignment-04.py
+++ ITMD-521/assignment-04/assignment-04.py
@@ -9,4 +9,5 @@ spark = SparkSession\
 .config('spark.driver.extraClassPath', '/home/vagrant/spark/jars/mysql-connector-java-8.0.28.jar')\
 .getOrCreate()
 
-JDBC_DF = spark.read.format("jdbc").option("url","jdbc:mysql://localhost/employees").option("driver","com.mysql.jdbc.Driver").option("dbtable","employees").option("user","worker").option("password","cluster").load().show()
+JDBC_DF = spark.read.format("jdbc").option("url","jdbc:mysql://localhost/employees").option("driver","com.mysql.jdbc.Driver").option("dbtable","employees").option("user","worker").option("password","cluster").load().createOrReplaceTempView("Employees")
+
