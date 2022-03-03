9faa248b17ad944e30c08152631ef92a73b7ab27
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-03-02 22:06:49 -0600

    assignment 4 Q1

--- ITMD-521/assignment-04/assignment-04.py
+++ ITMD-521/assignment-04/assignment-04.py
@@ -11,3 +11,7 @@ spark = SparkSession\
 
 JDBC_DF = spark.read.format("jdbc").option("url","jdbc:mysql://localhost/employees").option("driver","com.mysql.jdbc.Driver").option("dbtable","employees").option("user","worker").option("password","cluster").load().createOrReplaceTempView("Employees")
 
+#Assignment Part 1
+# Q1 - Display the count of the number of records in the DF
+Q1_DF=spark.sql("Select count(*) from Employees")
+Q1_DF.show()
