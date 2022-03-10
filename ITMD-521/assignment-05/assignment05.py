b56d67d19ad99cacc70cba222d57dc980525cdf0
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-03-09 21:00:28 -0600

    Assignment05- union query

--- ITMD-521/assignment-05/assignment05.py
+++ ITMD-521/assignment-05/assignment05.py
@@ -36,3 +36,4 @@ data_frame.createOrReplaceTempView("Assignment-05-TempView-B")
 # Unoin of Tables
 DF_Union = data_source.union(data_frame)
 DF_Union.show()
+DF_Union.createOrReplaceTempView("Union-Data-Frame")
