ff6e86cf863171dc18667deb06ab9f1361c880a2
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-03-09 21:02:28 -0600

    Assignment05- union query

--- ITMD-521/assignment-05/assignment05.py
+++ ITMD-521/assignment-05/assignment05.py
@@ -38,5 +38,6 @@ DF_Union = data_source.union(data_frame)
 DF_Union.show()
 DF_Union.createOrReplaceTempView("Union-Data-Frame")
 DF_Union_Query = DF_Union.select("ID",split(col("temperature"),",").alias("temperatures")).drop("temperature")
-
+DF_Union_Query.printSchema()
+DF_Union_Query.show()
 
