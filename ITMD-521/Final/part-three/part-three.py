1d17a3a67f80a05e4fe2c5d1537234af5e8eb941
Suraj Govind Rajulu <srajulu@hawk.iit.edu>
2022-04-30 16:53:31 -0500

    part three

--- ITMD-521/Final/part-three/part-three.py
+++ ITMD-521/Final/part-three/part-three.py
@@ -14,11 +14,21 @@ if __name__ == "__main__":
         print("Usage: part-three.py <file> ", file=sys.stderr)
         sys.exit(-1)
 
-    config = SparkConf()
-    config.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')
-    config.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
+    con = SparkConf()
+    con.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')
+    con.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider')
     
-    config.set('spark.hadoop.fs.s3a.access.key', os.getenv('SECRETKEY'))
-    config.set('spark.hadoop.fs.s3a.secret.key', os.getenv('ACCESSKEY'))
-    config.set("spark.hadoop.fs.s3a.endpoint", "http://192.168.172.50:9000")
+    con.set('spark.hadoop.fs.s3a.access.key', os.getenv('SECRETKEY'))
+    con.set('spark.hadoop.fs.s3a.secret.key', os.getenv('ACCESSKEY'))
+    con.set("spark.hadoop.fs.s3a.endpoint", "http://192.168.172.50:9000")
+
+    spark = SparkSession.builder.appName("suraj part three").config('spark.driver.host','192.168.172.45').config(conf=con).getOrCreate()
+
+    parquet_file = "s3a://srajulu/80.parquet"
+    spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
+
+    parquet_df = spark.read.format("parquet").option("header", "true").option("inferSchema", "true").load(parquet_file)
+    parquet_df.printSchema()
+    
+    
    
